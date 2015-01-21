dbf3c15de1c35485167e3c99ea192c34a53e039e
Jon <jonathan.tan83@gmail.com>
2015-01-20 17:11:02 -1000

    completed level 1

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,54 @@
  **/
 
 // Create an empty array.
-
+var array = [ 1, 2, 3, 4, 5];
 
 // Create an array of the colors of the rainbow.
-
+var rainbowColors = [ "blue", "red", "green", "yellow", "purple"];
 
 // Create an array of gems.
-
+var gems = [ "ruby", "sapphire"];
 
 // Create an array of military ranks.
-
+var militaryRank = [ "private", "staff sargent", "captain", "major", "lieutenant"];
 
 // Create an array of road signs.
-
+var roadSigns = [ "stop", "yield", "slow down"];
 
 // Create an array containing components of a tree.
-
+var treeComps = [ "roots", "trunk", "branch", "leaves"];
 
 // Create an array of prime numbers less than 20.
-
+var array = [ 2, 3, 5, 7, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
-
+var progWords = [ "wat", "shit", "mother", "dammit"];
 
 // Create an array of elements of the periodic table.
-
+var elements = [ "hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "Nitrogen"];
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = [ "nickel", "dime", "quarter", "lint"];
 
 // Create an array of items you bring to class daily.
-
+var dailyItems = [ "cup", "keys", "lunchbox", "computer", "rambo knife"];
 
 // Create an array of web developer role titles.
-
+var webDevRoles = [ "developer", "quality tester", "doc writer", "requirement manager"];
 
 // Create an array of good luck charms.
-
+var luckCharms = [ "neighbors fingers", "sandy's teeth", "ear necklace", "grandma's fingernails"];
 
 // Create an array of mythical animals.
-
+var mythicalAnimals = ["jon tan", "sabretooth tiger", "ninja turtle", "amy from congo"];
 
 // Create an array of drinks.
-
+var drinks = [ "soda", "cocktails", "capri sun", "r kelly's urine"];
 
 // Create an array of emoticons.
-
+var emoticon = [ ":)", ";)", "8===D"];
 
 // Create an array with the letters of the word 'array'.
-
+var letters = [ "a", "r", "r", "a", "y" ];
 
 
