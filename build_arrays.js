aa0fd8052ce3e004c9edd97532088998f6712b43
Jon <jonathan.tan83@gmail.com>
2015-01-20 18:47:02 -1000

    level 2 complete

--- build_arrays.js
+++ build_arrays.js
@@ -3,55 +3,55 @@
  **/
 
 // Create an empty array.
-
+var array = [ 1, 2, 3, 4, 5];
 
 // Create an array of the colors of the rainbow.
-
+var rainbowColors = [ "blue", "red", "green", "yellow", "purple"];
 
 // Create an array of gems.
-
+var gems = [ "ruby", "sapphire"];
 
 // Create an array of military ranks.
-
+var militaryRank = [ "private", "staff sargent", "captain", "major", "lieutenant"];
 
 // Create an array of road signs.
-
+var roadSigns = [ "stop", "yield", "slow down"];
 
 // Create an array containing components of a tree.
-
+var treeComps = [ "roots", "trunk", "branch", "leaves"];
 
 // Create an array of prime numbers less than 20.
-
+var array = [ 2, 3, 5, 7, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
-
+var progWords = [ "wat", "shit", "mother", "dammit"];
 
 // Create an array of elements of the periodic table.
-
+var elements = [ "hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "Nitrogen"];
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = [ "nickel", "dime", "quarter", "lint"];
 
 // Create an array of items you bring to class daily.
-
+var dailyItems = [ "cup", "keys", "lunchbox", "computer", "rambo knife"];
 
 // Create an array of web developer role titles.
-
+var webDevRoles = [ "developer", "quality tester", "doc writer", "requirement manager"];
 
 // Create an array of good luck charms.
-
+var luckCharms = [ "neighbors fingers", "sandy's teeth", "ear necklace", "grandma's fingernails"];
 
 // Create an array of mythical animals.
-
+var mythicalAnimals = ["jon tan", "sabretooth tiger", "ninja turtle", "amy from congo"];
 
 // Create an array of drinks.
-
+var drinks = [ "soda", "cocktails", "capri sun", "r kelly's urine"];
 
 // Create an array of emoticons.
-
+var emoticon = [ ":)", ";)", "8===D"];
 
 // Create an array with the letters of the word 'array'.
-
+var letters = [ "a", "r", "r", "a", "y" ];
 
 
 
@@ -61,42 +61,21 @@
 
 // Create an array of Objects that represent Famous People.
 // Each Object should have a Name, Age, and their Profession as properties
-
+var famous = {president : "lincoln", actor : "jim carey", presidentwife : "nancy reagan", comedian : "bob hope"};
 
 // Create an array of songs you'd like Jon to add to the playlist of music 
 // to listen to in class.  Each song should be an object with at least 
 // a title and an artist.
-
+var songs = { eltonJohn : "rocket man", nirvana : "smells like teen spirit", kanye : "diamonds", rjd2 : "ghostwriter"};
 
 // Create an array of functions. Define any functions you want, they can 
 // be anything! But make sure the syntax is correct :D
-
+var functions = { fun1 : function1(), fun2 : function2(), fun3 : function3()};
 
 // Create an array of Sublime shortcuts.  Each item should have a keystroke 
 // combination and a textual description of what the key combination does. 
 // You can find Sublime Text Shortcuts by traversing the Sublime Text's GUI
 // e.g. File Menu options.
-
-
-
-
-/*
- * LEVEL 3 - Nested Arrays
- **/
-
-// Create an array of gem arrays, where each gem array contains gems that 
-// are of a similar color.
-
-
-// Create an array of aisles at a supermarket.  Each aisle is an array of 
-// items you might find in an aisle together.  (e.g. flour and baking 
-// powder would be found in an aisle together so would Apples and Bananas.)
-
-
-// Create an array of three arrays, each array having numbers that add up 
-// to 15, but having no element listed more than once in any array.
-// e.g. each element can only be in one array, and at most once in each array
-
-
+var shortcuts = { load : "subl .", find : "cmd F", select : "shift and arrows"};
 
 
